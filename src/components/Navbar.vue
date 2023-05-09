7147b4c8c01f1a70db3e0d8a4f9af9bdaf4e806f
Mardin <mardin.ezatpana@gmail.com>
2023-05-09 16:42:09 +0330

    add navbar to project

---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="bg-white shadow h-16 flex justify-between items-stretch">
+    <router-link
+      :to="{ name: 'home' }"
+      class="inline-flex items-center h-full px-5"
+    >
+      Home
+    </router-link>
+    <div class="flex items-center gap-1">
+      <router-link
+        to="/byName"
+        class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100"
+      >
+        Search Meals
+      </router-link>
+      <router-link
+        to="/byLetter"
+        class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100"
+      >
+        Meals By Letter
+      </router-link>
+      <router-link
+        to="/byIngredient"
+        class="inline-flex items-center px-2 h-full transition-colors hover:bg-purple-100"
+      >
+        Meals By Ingredients
+      </router-link>
+    </div>
+  </header>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex flex-col p-8">
+    <div class="p-8 pb-0">
+    <input
+      type="text"
+      class="rounded border-2 bg-white border-gray-200 w-full"
+      placeholder="Search for Meals"
+    />
+  </div>
+</div>
+</template>
+
+<script setup>
+</script>
